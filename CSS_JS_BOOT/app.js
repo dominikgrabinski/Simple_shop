dfdd26012a432fe5d194c88462e8d04b9795b2bb
Michał Stanek <michal.stanek@me.com>
2017-03-28 15:20:38 +0200

    dodane rejestracja w JS - po kliknięciu zostajemy na tej samej stronie tylko znika okno logowania a pojawia się rejstracji - załóż konto

--- CSS_JS_BOOT/app.js
+++ CSS_JS_BOOT/app.js
@@ -3,19 +3,33 @@ $("#login_button").click(function(){
 
 });
 
-$(".close").click(function(){
-    $("#logowanie_box").fadeOut();
 
-  });
   
 
 $(document).mouseup(function (e)
 {
     var container = $("#logowanie_box");
+    var container1 = $(".modal-content");
 
-    if (!container.is(e.target) 
-        && container.has(e.target).length === 0)
+    if ((!container.is(e.target)&&(!container1.is(e.target))
+        && container.has(e.target).length === 0 && container1.has(e.target).length === 0))
     {
         container.fadeOut();
+        container1.fadeOut();
     }
 });
+
+
+$(".btn-primary").click(function(){
+   
+    $(".modal-content").fadeIn("slow");
+    $("#logowanie_box").hide();
+     
+
+});
+
+$(".close").click(function(){
+     $("#logowanie_box").hide();
+     $(".modal-content").fadeOut("slow");
+
+  });
