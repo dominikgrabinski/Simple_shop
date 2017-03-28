9713bab64ef3e1ca979e13dd6e2d4f1a0f104af9
Michał Stanek <michal.stanek@me.com>
2017-03-28 15:37:40 +0200

    dodane rejestracja w JS - po kliknięciu zostajemy na tej samej stronie tylko znika okno logowania a pojawia się rejstracji - załóż konto v.2 + dodanie kolejego przycisku + koszyka

---
+++ Sites/basket.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>koszyk</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link href="../CSS_JS_BOOT/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="../CSS_JS_BOOT/main.css" type="text/css">
+    </head>
+    <body">
+  
+        
+        <?php
+        echo "<pre>    Tu będzie Twój koszyk</pre>"
+        ?>
+        
+        
+        
+        <script src="../CSS_JS_BOOT/jquery.min.js" type="text/javascript">  </script>
+        <script src="../CSS_JS_BOOT/jquery.js" type="text/javascript">  </script>
+        <script src="../CSS_JS_BOOT/bootstrap.min.js" type="text/javascript">  </script>
+        <script src="../CSS_JS_BOOT/app.js" type="text/javascript">  </script>
+        
+    </body>
+</html>
