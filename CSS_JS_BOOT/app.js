efea5cd7ac2254637b4c76c774c3c2f187989b36
Michał Stanek <michal.stanek@me.com>
2017-03-28 11:18:30 +0200

    klikniecie nie w DIVA zamyka Diva login

--- CSS_JS_BOOT/app.js
+++ CSS_JS_BOOT/app.js
@@ -6,4 +6,16 @@ $("#login_button").click(function(){
 $(".close").click(function(){
     $("#logowanie_box").fadeOut();
 
+  });
+  
+
+$(document).mouseup(function (e)
+{
+    var container = $("#logowanie_box");
+
+    if (!container.is(e.target) 
+        && container.has(e.target).length === 0)
+    {
+        container.fadeOut();
+    }
 });
