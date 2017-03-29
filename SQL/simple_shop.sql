19f5249b63ace76aef28f96854c9febbae3a9a75
dominikgrabinski <grabinski.dominik@gmail.com>
2017-03-29 13:12:24 +0200

    uzupełniona BD

--- SQL/simple_shop.sql
+++ SQL/simple_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Czas generowania: 27 Mar 2017, 15:09
+-- Czas generowania: 29 Mar 2017, 13:10
 -- Wersja serwera: 5.7.17
 -- Wersja PHP: 7.0.15-0ubuntu0.16.04.4
 
@@ -19,6 +19,80 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `simple_shop`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `Admin`
+--
+
+CREATE TABLE `Admin` (
+  `id` int(255) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `Magazine`
+--
+
+CREATE TABLE `Magazine` (
+  `id` int(255) NOT NULL,
+  `Products_id` int(255) NOT NULL,
+  `Orders_id` int(255) NOT NULL,
+  `dostepnosc` varchar(20) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `liczba_w_magazynie` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `Orders`
+--
+
+CREATE TABLE `Orders` (
+  `id` int(255) NOT NULL,
+  `User_id` int(255) NOT NULL,
+  `Status_id` int(255) NOT NULL,
+  `adres_zamownienia` varchar(255) NOT NULL,
+  `suma_do_zaplaty` decimal(6,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `Products`
+--
+
+CREATE TABLE `Products` (
+  `id` int(11) NOT NULL,
+  `tytul` varchar(255) NOT NULL,
+  `platfotma` varchar(50) NOT NULL,
+  `gatunek` varchar(30) NOT NULL,
+  `opis` varchar(255) NOT NULL,
+  `cena` decimal(5,2) NOT NULL,
+  `kategoria_wiekowa` varchar(30) NOT NULL,
+  `wydawca` varchar(50) NOT NULL,
+  `jezyk` varchar(20) NOT NULL,
+  `data_premiery` datetime NOT NULL,
+  `promocja` varchar(50) NOT NULL,
+  `edycja` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `Status`
+--
+
+CREATE TABLE `Status` (
+  `id` int(255) NOT NULL,
+  `status_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -41,6 +115,42 @@ CREATE TABLE `Users` (
 --
 
 --
+-- Indexes for table `Admin`
+--
+ALTER TABLE `Admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `Magazine`
+--
+ALTER TABLE `Magazine`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Products_id` (`Products_id`),
+  ADD KEY `Orders_id` (`Orders_id`);
+
+--
+-- Indexes for table `Orders`
+--
+ALTER TABLE `Orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `User_id` (`User_id`),
+  ADD KEY `Status_id` (`Status_id`);
+
+--
+-- Indexes for table `Products`
+--
+ALTER TABLE `Products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Status`
+--
+ALTER TABLE `Status`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `Users`
 --
 ALTER TABLE `Users`
@@ -53,10 +163,53 @@ ALTER TABLE `Users`
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `Admin`
+--
+ALTER TABLE `Admin`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `Magazine`
+--
+ALTER TABLE `Magazine`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `Orders`
+--
+ALTER TABLE `Orders`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `Products`
+--
+ALTER TABLE `Products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `Status`
+--
+ALTER TABLE `Status`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT dla tabeli `Users`
 --
 ALTER TABLE `Users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `Magazine`
+--
+ALTER TABLE `Magazine`
+  ADD CONSTRAINT `Magazine_ibfk_1` FOREIGN KEY (`Products_id`) REFERENCES `Products` (`id`),
+  ADD CONSTRAINT `Magazine_ibfk_2` FOREIGN KEY (`Orders_id`) REFERENCES `Orders` (`id`);
+
+--
+-- Ograniczenia dla tabeli `Orders`
+--
+ALTER TABLE `Orders`
+  ADD CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`User_id`) REFERENCES `Users` (`id`),
+  ADD CONSTRAINT `Orders_ibfk_2` FOREIGN KEY (`Status_id`) REFERENCES `Status` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
