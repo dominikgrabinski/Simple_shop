fca314b3d219545c06f99102d324567417fa4eac
Michał Stanek <michal.stanek@me.com>
2017-03-29 13:30:15 +0200

    verify login - nie działa prace trwaja nad poprawnym zapytaniem SQL i salt

--- Verify/login_check.php
+++ Verify/login_check.php
@@ -1,9 +1,47 @@
 <?php
+require_once '../SRC/config.php';
+require_once '../Class/User.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+session_start();
 
-echo "login";
+  if(!empty($_POST['email']))
+  {
+
+    if (!empty($_POST['password']))
+    {
+      $user = new User();
+      $pass = $user->setPassword($_POST['password']);
+
+      $query="SELECT COUNT(*) as ilosc,id,username FROM Users WHERE email='".$_POST['email']."' AND hashed_password='$pass'";
+      $result = $connection->query($query);
+      $row = $result->fetch_assoc();
+      if($row['ilosc'] == 1 )
+      {
+        // session_regenerate_id();
+        //$_SESSION['userIP'] = $_SERVER['REMOTE_IP'];
+        $_SESSION['userID'] = $row['id'];
+        $_SESSION['username'] = $row['username'];
+        echo "Trwa przekierowanie na strone główna";
+        header( "refresh:3;url=glowna.php" );
+      }
+      else
+      {
+        echo "NIET";
+        header( "refresh:2;url=loginhtml.php" );
+
+      }
+    }
+    else
+    {
+    echo "Podaj hasło!";
+    header( "refresh:3;url=loginhtml.php" );
+    }
+  }
+  else
+  {
+    echo "Podaj email!";
+    header( "refresh:3;url=loginhtml.php" );
+  }
+
+  ?>
+
